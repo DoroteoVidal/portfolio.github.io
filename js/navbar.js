e47b1a332c716c6ed96ffd845384b57753f89460
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-06-02 10:12:15 -0300

    reduccion de consumo de recursos para renderizar archivos html

--- js/navbar.js
+++ js/navbar.js
@@ -2,29 +2,60 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#hello").classList.add("selected");
-    showHtml("html/hello.html");
+    showHtml("html/hello.html", "#content");
 
-    function selectTab(id) {
+    function selectTab(id, element, cssClass) {
         document    
-            .querySelectorAll('.navbar-item')
-            .forEach((item) => item.classList.remove("selected"));
+            .querySelectorAll(element)
+            .forEach((item) => item.classList.remove(cssClass));   
+
         document    
-            .querySelectorAll('#' + id) 
-            .forEach((item) => item.classList.add("selected")); 
+            .querySelectorAll("#" + id) 
+            .forEach((item) => item.classList.add(cssClass)); 
     }
 
-    function push(event) {
+    function push(event, element, cssClass, container) {
         let id = event.target.id;
-        selectTab(id);
-        document.tittle = id;
-        showHtml(`html/${id}.html`);
+        selectTab(id, element, cssClass);
+        showHtml(`html/${id}.html`, container);
     }
 
-    window.onload = (event) => {
-        document.querySelector("#hello").addEventListener("click", (event) => push(event));
-        document.querySelector("#about").addEventListener("click", (event) => push(event));
-        document.querySelector("#projects").addEventListener("click", (event) => push(event));
+    document.body.addEventListener("click", function(event) {
+        if(event.target.matches("#hello, #about, #projects")) {
+            push(event, ".navbar-item", "selected", "#content");
+        } else if(event.target.matches("#biography, #experience, #education, #skills")) {
+            push(event, ".aside-about-item", "aside-about-selected", "#about-content");
+        }
+    });
+
+    /*function addListeners() {
+        const hello = document.querySelector("#hello");
+        const about = document.querySelector("#about");
+        const projects = document.querySelector("#projects");
+        const biography = document.querySelector("#biography");
+        const experience = document.querySelector("#experience");
+        const education = document.querySelector("#education");
+        const skills = document.querySelector("#skills");
+
+        if(hello) hello.addEventListener("click", (event) => push(event, "#content"));
+        if(about) about.addEventListener("click", (event) => push(event, "#content"));
+        if(projects) projects.addEventListener("click", (event) => push(event, "#content"));
+        if(biography) biography.addEventListener("click", (event) => push(event, "#about-content"));
+        if(experience) experience.addEventListener("click", (event) => push(event, "#about-content"));
+        if(education) education.addEventListener("click", (event) => push(event, "#about-content"));
+        if(skills) skills.addEventListener("click", (event) => push(event, "#about-content"));
     }
 
-    
+    addListeners();
+
+    const observer = new MutationObserver((mutations) => {
+        for(let mutation of mutations) {
+            if(mutation.type === "childList") {
+                addListeners();
+            }
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });*/
+
 });
--- js/showHTML.js
+++ js/showHTML.js
@@ -1,15 +1,15 @@
-function showHtml(path) {
+function showHtml(path, container) {
     fetch(path)
             .then(response => {
                 if(!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
+                    throw new Error("Network response was not ok " + response.statusText);
                 }
                 return response.text();
             })
             .then(data => {
-                document.querySelector('#content').innerHTML = data;
+                document.querySelector(container).innerHTML = data;
             })
             .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
+                console.error("There was a problem with the fetch operation:", error);
             });
 }
