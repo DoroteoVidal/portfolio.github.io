ea8ded87e4fc4af5a1d053646faa82eb96afe2bb
Doroteo Vidal <doroteo17vidal@hotmail.com>
2024-06-03 14:45:26 -0300

    Refactor metodo mostrar primero al entrar a seccion about y projects

--- js/navbar.js
+++ js/navbar.js
@@ -21,28 +21,32 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     document.body.addEventListener("click", function(event) {
-        if(event.target.matches("#hello, #projects")) {
+        if(event.target.matches("#hello")) {
             push(event, ".navbar-item", "selected", "#content");
+
+        } else if(event.target.matches("#projects")) {
+            push(event, ".navbar-item", "selected", "#content");
+            showFirst("html/projects.html", "html/projects/scooters.html", "eCommerce", "#project-content");
 
         } else if(event.target.matches("#about")) {
             push(event, ".navbar-item", "selected", "#content");
-            showBiographyFirst();
+            showFirst("html/about.html", "html/biography.html", "biography", "#about-content");
 
         } else if(event.target.matches("#biography, #experience, #education, #skills")) {
-            push(event, ".aside-about-item", "aside-about-selected", "#about-content");
+            push(event, ".aside-item", "aside-selected", "#about-content");
         }
     });
 
-    function showBiographyFirst() {
-        fetch("html/about.html").then(response => {
+    function showFirst(path, htmlFile, id, container) {
+        fetch(path).then(response => {
             if(!response.ok) {
                 throw new Error("Network response was not ok " + response.statusText);
             }
             return response.text();
             
         }).then(() => {
-            selectTab("biography", ".aside-about-item", "aside-about-selected");
-            showHtml(`html/biography.html`, "#about-content");
+            selectTab(id, ".aside-item", "aside-selected");
+            showHtml(htmlFile, container);
         });
        
         
